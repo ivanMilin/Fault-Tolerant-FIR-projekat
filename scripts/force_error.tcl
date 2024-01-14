0f1f35b1136d589fb7ead1e224bbcf43221ec89d
ivanMilin <ivan.milin32@gmail.com>
2024-01-14 18:52:12 +0100

    Added files for forcing and removing errors

---
+++ scripts/force_error.tcl
@@ -0,0 +1,6 @@
+add_force {/tb/uut_fir_filter/\replication_of_fir(0)\/replication/first_data_o_s[10]} -radix hex {1 400ns} -cancel_after 400ms
+add_force {/tb/uut_fir_filter/\replication_of_fir(1)\/replication/first_data_o_s[10]} -radix hex {1 1000ns} -cancel_after 400ms
+add_force {/tb/uut_fir_filter/\replication_of_fir(2)\/replication/first_data_o_s[10]} -radix hex {1 2000ns} -cancel_after 400ms
+add_force {/tb/uut_fir_filter/\replication_of_fir(3)\/replication/first_data_o_s[10]} -radix hex {1 4000ns} -cancel_after 400ms
+add_force {/tb/uut_fir_filter/\replication_of_fir(4)\/replication/first_data_o_s[10]} -radix hex {1 5000ns} -cancel_after 400ms
+add_force {/tb/uut_fir_filter/\replication_of_fir(5)\/replication/first_data_o_s[10]} -radix hex {1 6000ns} -cancel_after 400ms
---
+++ scripts/remove_force.tcl
@@ -0,0 +1,6 @@
+remove_forces { {/tb/uut_fir_filter/\replication_of_fir(0)\/replication/first_data_o_s} }
+remove_forces { {/tb/uut_fir_filter/\replication_of_fir(1)\/replication/first_data_o_s} }
+remove_forces { {/tb/uut_fir_filter/\replication_of_fir(2)\/replication/first_data_o_s} }
+remove_forces { {/tb/uut_fir_filter/\replication_of_fir(3)\/replication/first_data_o_s} }
+remove_forces { {/tb/uut_fir_filter/\replication_of_fir(4)\/replication/first_data_o_s} }
+remove_forces { {/tb/uut_fir_filter/\replication_of_fir(5)\/replication/first_data_o_s} }
